438f258f5d692b5a96630bd4833966f1ae2c677e
VasilyZa <450270247@qq.com>
2026-07-31 11:39:22 +0800

    fix: correct version display and branding
    - Remove hardcoded 1.21.1 from welcome message (en/ru/fr/es)
    - Metrics banner_version now reads real version dynamically
    - Update QQ group to 486029013
    - Rebrand development team to Yanyang Technology Group (2nd Modified)

--- modules/taiyitist-server/src/main/java/org/teneted/taiyitist/TaiyitistMCStart.java
+++ modules/taiyitist-server/src/main/java/org/teneted/taiyitist/TaiyitistMCStart.java
@@ -31,7 +31,7 @@ public class TaiyitistMCStart {
             LOGGER.info("{} {}, Java {}", I18n.as("taiyitist.launch.welcomemessage"), getVersion(), javaVersion);
         }
         if(I18N.isCN()) {
-            System.out.printf("官方交流QQ群: 211128424%n");
+            System.out.printf("官方交流QQ群: 486029013%n");
             System.out.printf("如果控制台出现中文乱码请添加启动参数: -Dfile.encoding=GBK%n");
         }
         if (!EulaUtil.hasAcceptedEULA()) {
